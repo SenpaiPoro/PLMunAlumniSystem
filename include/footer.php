c05ed776b2e06b4fe02a4dc57f90e80405c6ebbf
Marlon Mullet <marlonmullet10@gmail.com>
2025-03-16 18:43:47 +0800

    init

--- include/footer.php
+++ include/footer.php
@@ -7,8 +7,8 @@
                   document.write(new Date().getFullYear())
                 </script>,
                 made with <i class="fa fa-heart"></i> by
-                <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
-                for a better web.
+                <a href="#" class="font-weight-bold" target="_blank">BSCS-3A</a>
+                for PLMun Alumni System
               </div>
             </div>
             <div class="col-lg-6">
