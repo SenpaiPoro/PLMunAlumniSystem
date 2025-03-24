4969ba3f3b835b871f62ccd169e09bbbf597e554
Marlon Mullet <marlonmullet10@gmail.com>
2025-03-25 04:00:42 +0800

    graph in index for the super admin

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php include ('./include/header.php'); ?>
 
 
-
+<div class="chart-container">
+  <div class="chart-box">
+    <h3>All Users</h3>
+    <canvas id="allUsersChart"></canvas>
+  </div>
+</div>
  
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="../config/chartscript.js"></script>
 <?php include ('./include/footer.php'); ?>
