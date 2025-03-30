61ec46a7a867bfbcb95215da993c979e460e8577
Marlon Jr Mullet <marlonmullet10@gmail.com>
2025-03-30 23:36:06 +0800

    first

--- Home_Settings.php
+++ Home_Settings.php
@@ -1,16 +1,34 @@
 <?php include ('include/header.php'); ?>
+
+<?php $selectedCollege = isset($_GET['college']) ? validate($_GET['college']) : null; ?>
+
 <div class="row">
     <div class="col-md-12">
         <div class="card">
             <div class="card-header">
                 <h4>
                   Alumni Lists
-                    <a href="Home_Management.php" class="btn btn-primary float-end"> Add Almuni </a>
+                    <a href="Home_Management.php" class="btn btn-primary float-end"> Add Alumni </a>
                 </h4> 
             </div>
         </div>
+
         <div class="card-body">
-        <?= alertMessage(); ?>
+            <!-- Filter Form -->
+            <form method="GET" action="">
+                <label for="college">Filter by College:</label>
+                <select name="college" id="college" class="form-select" onchange="this.form.submit()">
+                    <option value="">Show All</option>
+                    <option value="CITCS" <?= $selectedCollege == 'CITCS' ? 'selected' : '' ?>>CITCS</option>
+                    <option value="CCJ" <?= $selectedCollege == 'CCJ' ? 'selected' : '' ?>>CCJ</option>
+                    <option value="CAS" <?= $selectedCollege == 'CAS' ? 'selected' : '' ?>>CAS</option>
+                    <option value="CBA" <?= $selectedCollege == 'CBA' ? 'selected' : '' ?>>CBA</option>
+                    <option value="CTE" <?= $selectedCollege == 'CTE' ? 'selected' : '' ?>>CTE</option>
+                </select>
+            </form>
+
+            <!-- Table -->
+            <?= alertMessage(); ?>
             <table class="table table-bordered table-striped">
                 <thead>
                     <tr>
@@ -23,21 +41,20 @@
                 </thead>
                 <tbody>
                  <?php
-                    $users = GetData('users');
+                    $users = GetData('users', $selectedCollege);
                     if(mysqli_num_rows($users) > 0 )
                     {
-                        foreach($users as $usersList)
+                        while($usersList = mysqli_fetch_assoc($users))
                         {
                             ?>
                               <tr> 
-                                <!--  htmlspecialchars  Prevents XSS Cross-Site Scripting  -->
                                     <td> <?= htmlspecialchars($usersList['level']); ?></td>
-                                    <td> <?= htmlspecialchars($usersList['colleges']); ?></td>
+                                    <td> <?= htmlspecialchars($usersList['program']); ?></td>
                                     <td> <?= htmlspecialchars($usersList['username']); ?></td>
                                     <td> <?= htmlspecialchars($usersList['tempcode']); ?></td>
                                 <td> 
                                 <a href="Home_Edit.php?id=<?= $usersList['id'];?>&level=superadmin" class="btn btn-success btn-sm">Edit</a>
-                                <a href="user-delete.php?id=<?= $usersList['id'];?>&level=superadmin" class="btn btn-danger btn-sm" onclick="return confirm('Are you Sure that you want to delete this User?');">Delete</a>
+                                <a href="user-delete.php?id=<?= $usersList['id'];?>&level=superadmin" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this user?');">Delete</a>
                                 <a href="View-pfp.php?id=<?= $usersList['id'];?>" class="btn btn-info btn-sm">View</a>
                             </td>
                     </tr>
@@ -48,7 +65,7 @@
                     {
                         ?>
                             <tr>
-                                <td colspan="4">
+                                <td colspan="5">
                                     No Record!
                                 </td>
                             </tr>
