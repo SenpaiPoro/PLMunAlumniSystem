e264821a56810ed874be07f6b9cc547564f302f4
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-07-03 22:45:30 +0800

    new feature

--- Home_Management.php
+++ Home_Management.php
@@ -65,14 +65,11 @@
                         </select>
                         <label>Code</label>
                         <input type="number" name="tempcode" placeholder="Enter Code" class="form-control">
-
                         <!------------------------------------------------------------------------------------>
                         <!------this is only to identy if the form is sumbited by the super admin------------->
                         <input type="hidden" name="superadmin" value="1">
                         <!------------------------------------------------------------------------------------>
                         <!------------------------------------------------------------------------------------>
-
-
                     </div>
                     <!--------------------------------->
                     <!-- Personal Information fields -->
